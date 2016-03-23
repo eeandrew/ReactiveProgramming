cdc609e0ed57faaffb0d662628d75336507ca41d
zhoulin <zhoulin@zhoulin900.local>
2016-03-23 15:53:33 +0800

    [RX]使用startWith替换startupRequestStream

--- public/script.js
+++ public/script.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){
 	var REQUEST_URL = 'http://localhost:8000/users';
 
-	var startupRequestStream = Rx.Observable.just(REQUEST_URL);
-
 	var refreshClickStream = Rx.Observable.fromEvent($('.refresh-btn'),'click');
 
-	var requestOnRefreshStream = refreshClickStream.map(function(){
+	var requestStream = refreshClickStream
+	.startWith('start click')
+	.map(function(){
 		return REQUEST_URL;
 	});
-
-	var requestStream = Rx.Observable.merge(startupRequestStream,requestOnRefreshStream);
 
 	var responseStream = requestStream.flatMap(function(url){
 		return Rx.Observable.fromPromise($.getJSON(url))
@@ -18,8 +16,5 @@ $(document).ready(function(){
 	responseStream.subscribe(function(resp){
 		console.log(resp);
 	});
-
-	
-
 
 });

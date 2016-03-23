ab6d34fc30052f9644d4d004fd53ee73bf1ebaea
zhoulin <zhoulin@zhoulin900.local>
2016-03-23 17:05:29 +0800

    [RX] 新增一个简单的UI

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var REQUEST_URL = 'http://localhost:8000/users';
+	var REQUEST_URL = 'https://api.github.com/users';
 
 	var refreshClickStream = Rx.Observable.fromEvent($('.refresh-btn'),'click');
 
@@ -13,8 +13,57 @@ $(document).ready(function(){
 		return Rx.Observable.fromPromise($.getJSON(url))
 	});
 
-	responseStream.subscribe(function(resp){
-		console.log(resp);
+	var suggestion1Stream = responseStream.map(function(listUsers){
+		return listUsers[Math.floor(Math.random()*listUsers.length)];
+	}).merge(refreshClickStream.map(function(){
+		return null;
+	}));
+
+	var suggestion2Stream = responseStream.map(function(listUsers){
+		return listUsers[Math.floor(Math.random()*listUsers.length)];
+	}).merge(refreshClickStream.map(function(){
+		return null;
+	}));
+
+	var suggestion3Stream = responseStream.map(function(listUsers){
+		return listUsers[Math.floor(Math.random()*listUsers.length)];
+	}).merge(refreshClickStream.map(function(){
+		return null;
+	}));
+
+	suggestion1Stream.subscribe(function(user){
+		if(user) {
+			updateItem(user,0);
+		}else {
+
+		}
 	});
+
+	suggestion2Stream.subscribe(function(user){
+		if(user) {
+			updateItem(user,1);
+		}else {
+
+		}
+	});
+
+
+	suggestion3Stream.subscribe(function(user){
+		if(user) {
+			updateItem(user,2);
+		}else {
+
+		}
+	});
+
+	function updateItem(user,index) {
+		if($('.user-list > .list-item').eq(index).length > 0) {
+			$('.user-list > .list-item').eq(index).html(user.login);
+		}else {
+			var listDom = $('<div class="list-item"></div>');
+			listDom.html(user.login);
+			$('.user-list').append(listDom);
+		}
+	}
 
 });
